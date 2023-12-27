cd59024ff38d30ec295006dbd095f2d623b8f7ed
Levi Woodard <92559124+WoodardDigital@users.noreply.github.com>
2023-12-27 09:31:21 -0700

    Added sub IP work, type, few other odds and ends.

--- stackscript.sh
+++ stackscript.sh
@@ -6,7 +6,7 @@
 
 # install packages
 apt-get update 
-apt-get install -y ansible git python3 python3-pip
+apt-get install -y ansible git python3 python3-pip jq
 
 # installs ansible dependencies 
 sudo -H pip3 install -Iv 'resolvelib<0.6.0'
@@ -29,8 +29,12 @@ export REGION=$(curl -s -H "Metadata-Token: $MTOKEN" http://169.254.169.254/v1/i
 # Grabs our IP address just to have for now 
 export CONTROL_IP=$(curl -s -H "Metadata-Token: $MTOKEN" http://169.254.169.254/v1/network | grep "ipv4.public" | awk -F": " '{print $2}' | xargs)
 
+# Grabs our current plan size to duplicate for the new instance. 
+export TYPE=$(curl -s -H "Metadata-Token: $MTOKEN" http://169.254.169.254/v1/instance | grep "type" | awk -F": " '{print $2}' | xargs)
+
+
 # Makes a dir for our stuff
-mkdir -p /installation
+mkdir -p /tmp/installation
 
 # export our variables to a file 
 echo "$REGION" > /tmp/installation/REGION
@@ -38,18 +42,54 @@ echo "$CONTROL_IP" > /tmp/installation/CONTROL_IP
 echo "$MTOKEN" > /tmp/installation/MTOKEN
 echo "$API_TOKEN_PASSWORD" > /tmp/installation/API_TOKEN_PASSWORD
 echo "$SUBNODE_PASSWORD" > /tmp/installation/SUBNODE_PASSWORD
+echo "$TYPE" > /tmp/installation/TYPE
+
+# Define a mapping between regions (DCs) and IDs
+declare -A DCID_MAPPING
+DCID_MAPPING["us-ord"]=18
+DCID_MAPPING["us-lax"]=30
+
+# Check if the REGION exists in the mapping
+if [ -n "${DCID_MAPPING[$REGION]}" ]; then
+  DCID="${DCID_MAPPING[$REGION]}"
+  echo "DCID: $DCID"
+else
+  echo "Region $REGION not found in the mapping"
+fi
+
 # echos the DCIC to a file 
-echo "$LINODE_DATACENTERID" > /tmp/installation/DCID
+echo "$DCID" > /tmp/installation/DCID
 
 #Clones the playbooks down to the system 
-git clone https://github.com/WoodardDigital/ansible-failover.git
+# git clone https://github.com/WoodardDigital/ansible-failover.git
+# cd ansible-failover/Ansible 
 
-cd ansible-failover/Ansible
-
-
-# These were formerly used, keeping here till I know I don't need them any longer.
 # Generates a random password. This will need some modiciation, but it should work for now. 
-# tr -dc 'A-Za-z0-9!"#$%&'\''()*+,-./:;<=>?@[\]^_`{|}~' </dev/urandom | head -c 28 > /installation/subnode.password
-# Exports the password to the $SUB_PASS variable 
-# export SUB_PASS=$(cat /installation/subnode.password)
+export SUBPASS=$(LC_ALL=C tr -dc '[:graph:]' </dev/urandom | head -c 32; echo)
+echo $SUBPASS > /tmp/installation/SUBPASS
 
+# Creates a new Linode in the same DC
+response=$(curl -H "Content-Type: application/json" \
+-H "Authorization: Bearer $API_TOKEN_PASSWORD" \
+-X POST -d '{
+    "authorized_users": [
+        "levi_woodard"
+    ],
+    "backups_enabled": false,
+    "booted": true,
+    "image": "linode/ubuntu22.04",
+    "label": "autofailover-test-01",
+    "private_ip": false,
+    "region": "'"$REGION"'",
+    "root_pass": "'"$SUBPASS"'",
+    "tags": [
+        "Failover"
+    ],
+    "type": "'"$TYPE"'"
+}' https://api.linode.com/v4/linode/instances)
+
+# Use jq to extract the IP address from the response
+SUBIP=$(echo "$response" | jq -r '.ipv4[0]')
+
+# Print the extracted IP address
+echo $SUBIP > /tmp/installation/SUBIP
